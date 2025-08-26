2f3b937273da3d37c91ca91b5f182ca8fc46acd5
JolanaB169 <Jola.T@seznam.cz>
2025-08-26 15:17:16 +0200

    Room adding view

--- room_booking/urls.py
+++ room_booking/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from .views import add_room
 
 
 
 urlpatterns = [
+    path('room/new', add_room, name='add_room'),
 
 
 
--- room_booking/views.py
+++ room_booking/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.http import HttpRequest
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import Room
 
-# Create your views here.
+def add_room(request: HttpRequest):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        capacity = request.POST.get("capacity")
+        projector = request.POST.get("projector") == 'on'
+
+        if not name:
+            messages.error(request, "Prosím zadej název místonosti.")
+        elif Room.objects.filter(name=name).exists():
+            messages.error(request, "Místnost s tímto názvem již existuje.")
+        elif not capacity.isdigit() or int(capacity) <= 0:
+            messages.error(request, "Kapacita musí být kladné číslo.")
+        else:
+            Room.objects.create(
+                name=name,
+                capacity=int(capacity),
+                projector=projector
+            )
+            messages.success(request, "Místnost přidána.")
+            return redirect("room_list")
+    return render(request, 'add_room.html')
+
+
+
+
